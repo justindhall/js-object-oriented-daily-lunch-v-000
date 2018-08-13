6591c250a58ccbea0c7cf87d44f571b9f4ed2028
Justin Hall <hall.justin.d@gmail.com>
2018-08-13 14:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -94,6 +94,14 @@ class Delivery{
   }
   
   meal(){
-    
+    return store.meals.find(meal => meal.id == this.mealId)
+  }
+  
+  customer(){
+    return store.customers.find(customer => customer.id == this.customerId)
+  }
+  
+  neighborhood(){
+    return store.neighborhoods.find(neighborhood => neighborhood.id == this.neighborhoodId)
   }
 }
