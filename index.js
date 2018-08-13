1884ccc66429641bc1532d1cc98de371dad1a948
Justin Hall <hall.justin.d@gmail.com>
2018-08-13 14:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0;
 let customerId = 0;
-let mealId = 0;
+let mealId = 1;
 let deliveryId = 0;
 
 class Neighborhood {
